cc6a2a63652838b52a830325efa4c8f37324bf7d
lufeng76 <viclf76@126.com>
2021-12-08 16:32:33 +0800

    vbng

--- dbt_run.sh
+++ dbt_run.sh
@@ -11,7 +11,6 @@ if [ $mode = "run" ]; then
     echo "dbt Mode is run"
     if [ $full_refresh = "True" ]; then
         echo "Doing a full refresh"
-<<<<<<< HEAD
         dbt run --target="$dbt_target" --select="$dbt_models" --vars="$dbt_vars"  --full-refresh
     else
         echo "Not doing a full refresh"
@@ -20,22 +19,9 @@ if [ $mode = "run" ]; then
 elif [ $mode = "debug" ]; then
     echo "dbt Mode is debug"
     dbt debug  --target="$dbt_target"
-=======
-        dbt run --target="$dbt_target" --select="$dbt_models" --vars="$dbt_vars" --profiles-dir=/app/profiles_dir --full-refresh
-    else
-        echo "Not doing a full refresh"
-        dbt run --target="$dbt_target" --select="$dbt_models" --vars="$dbt_vars" --profiles-dir=/app/profiles_dir
-    fi
-elif [ $mode = "debug" ]; then
-    echo "dbt Mode is debug"
-    dbt debug --profiles-dir=/app/profiles_dir --target="$dbt_target"
-<<<<<<< HEAD
->>>>>>> 61973d6 (change cloudbuild)
-=======
 elif [ $mode = "deps" ]; then
-    echo "dbt Mode is debug"
+    echo "dbt Mode is dept"
     dbt deps
->>>>>>> 437a8ad (hkt demo)
 elif [ $mode = "local" ]; then
     echo "dbt local run"
     dbt run --target="$dbt_target" --select="$dbt_models" --vars="$dbt_vars" --project-dir=/Users/lufengsh/Documents/GitHub/dbt-demo/demo
@@ -43,9 +29,10 @@ else
     echo "Incorrect dbt Mode. Nothing to do"
 fi
 
-# capture the exit code from the dbt run command
+# this is a test
 # so that the final exit code form removing virtualenv cmd doesn't get used by KubernetesPodOperator 
 exit_code=$?
 
 # rethrowing the exit code to KubernetesPodOperator
 exit $exit_code 
+
