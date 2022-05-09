ff502d9738db57b79e99af9318a415d4cb6efad5
lufeng76 <viclf76@126.com>
2022-05-09 15:32:27 +0800

    demo

---
+++ demo/analysis/gen_bank_yml.sql
@@ -0,0 +1,7 @@
+{{ codegen.generate_model_yaml(
+    model_name='stg_bank_account'
+) }}
+
+{{ codegen.generate_model_yaml(
+    model_name='stg_bank_base_card'
+) }}
---
+++ demo/analysis/stg_bank_base_card.sql
@@ -0,0 +1,4 @@
+{{ codegen.generate_base_model(
+    source_name='bank',
+    table_name='ext_base_card'
+) }}
